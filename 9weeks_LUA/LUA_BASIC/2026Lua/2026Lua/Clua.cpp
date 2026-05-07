d922e7e91879221ac99f449c32bbd7b2e8fd246c
SYJ1029 <abg0986@gmail.com>
2026-05-07 17:03:39 +0900

    LUA 파일을 읽어 실행

--- 9weeks_LUA/LUA_BASIC/2026Lua/2026Lua/Clua.cpp
+++ 9weeks_LUA/LUA_BASIC/2026Lua/2026Lua/Clua.cpp
@@ -3,18 +3,26 @@
 
 #pragma comment(lib, "lua55.lib")
 
-int main() {
+int main(void)
+{
+	int rows, cols;
+	lua_State* L = luaL_newstate(); //루아를연다.
+	luaL_openlibs(L); //루아표준라이브러리를연다.
+	luaL_loadfile(L, "dragon.lua");
+	int error = lua_pcall(L, 0, 0, 0);
 
-	const char* buff = "print \"Hello, Lua!\"";
-
-	lua_State* L = luaL_newstate();
-	luaL_openlibs(L);
-	luaL_loadbuffer(L, buff, strlen(buff), "line") || luaL_loadstring(L, buff);
-	int error = lua_pcall(L, 0, LUA_MULTRET, 0);
-	if (error) {
-		std::cerr << "Error executing Lua code: " << lua_tostring(L, -1) << std::endl;
-		lua_pop(L, 1); // Remove error message from stack
+	if (error)
+	{
+		std::cerr << "Error: " << lua_tostring(L, -1) << std::endl;
+		lua_pop(L, 1);
 	}
+
+	lua_getglobal(L, "pos_x");
+	lua_getglobal(L, "pos_y");
+	rows = (int)lua_tonumber(L, -2);
+	cols = (int)lua_tonumber(L, -1);
+	std::cout << "Rows " << rows << ", Cols " << cols << std::endl;
+	lua_pop(L, 2);
 	lua_close(L);
 	return 0;
 }
