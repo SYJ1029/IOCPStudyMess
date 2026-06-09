08d7a18e02b8d300b97a85b590bc4fe81daeac48
SYJ1029 <abg0986@gmail.com>
2026-06-09 11:47:41 +0900

    개선된 asio

--- 12weeks_ASIO/ASIO/SERVER/SERVER/npc_ai_lua.cpp
+++ 12weeks_ASIO/ASIO/SERVER/SERVER/npc_ai_lua.cpp
@@ -6,15 +6,13 @@
 #include <mutex>
 #include <unordered_set>
 #include <memory>
-#include <concurrent_unordered_map.h>
 #include <concurrent_priority_queue.h>
-#include <boost/unordered/concurrent_flat_map.hpp>
+#include <tbb/concurrent_unordered_map.h>
 #include "protocol.h"
 
-#include "include/lua.hpp"
-#include <boost/asio.hpp>
+#include <lua.hpp>
+#include <asio.hpp>
 
-#pragma comment(lib, "lua54.lib")
 using namespace std;
 
 constexpr int VIEW_RANGE = 5;
@@ -56,7 +54,7 @@ public:
 	S_STATE _state;
 	atomic_bool	_is_active;		// 주위에 플레이어가 있는가?
 	int _id;
-	boost::asio::ip::tcp::socket _socket;
+	asio::ip::tcp::socket _socket;
 	char	_recv_buff[NET_BUFF_SIZE];
 	int		_prev_remain;
 	short	x, y;
@@ -67,7 +65,7 @@ public:
 	lua_State*	_L;
 	mutex	_ll;
 public:
-	SESSION(boost::asio::ip::tcp::socket socket, int id) : _id(id), _socket(std::move(socket)), x(0), y(0), 
+	SESSION(asio::ip::tcp::socket socket, int id) : _id(id), _socket(std::move(socket)), x(0), y(0), 
 		_state(ST_FREE), _prev_remain(0)
 	{
 	}
@@ -97,11 +95,11 @@ public:
 	}
 	void do_recv()
 	{
-		_socket.async_read_some(boost::asio::buffer(_recv_buff + _prev_remain, sizeof(_recv_buff) - _prev_remain),
-			[this](boost::system::error_code ec, size_t bytes_read) {
+		_socket.async_read_some(asio::buffer(_recv_buff + _prev_remain, sizeof(_recv_buff) - _prev_remain),
+			[this](asio::error_code ec, size_t bytes_read) {
 				if (ec) {
-					if (ec.value() == boost::asio::error::operation_aborted) return;
-					cout << "Receive Error on Session[" << _id << "] EC[" << ec.what() << "]\n";
+					if (ec.value() == asio::error::operation_aborted) return;
+					cout << "Receive Error on Session[" << _id << "] EC[" << ec.message() << "]\n";
 					disconnect();
 					return;
 				}
@@ -114,7 +112,7 @@ public:
 	void do_send(void* packet)
 	{
 		size_t p_size = *reinterpret_cast<unsigned char*>(packet);
-		_socket.write_some(boost::asio::buffer(packet, p_size));
+		_socket.write_some(asio::buffer(packet, p_size));
 	}
 	void send_login_info_packet()
 	{
@@ -157,34 +155,25 @@ public:
 	void do_npc_random_move();
 };
 
-boost::concurrent_flat_map<int, shared_ptr<SESSION>> objects;
+tbb::concurrent_unordered_map<int, std::atomic<std::shared_ptr<SESSION>>> objects;
 
 void SESSION::disconnect()
 {
 	_vl.lock();
 	unordered_set <int> vl = _view_list;
 	_vl.unlock();
-	for (auto& p_id : vl) {
+	for	(const int &p_id : vl) {
 		if (is_npc(p_id)) continue;
-		int my_id = _id;
-		objects.visit(p_id, [my_id](auto &apl) {
-			shared_ptr<SESSION> pl = apl.second;
-			{
-				lock_guard<mutex> ll(pl->_s_lock);
-				if (ST_INGAME != pl->_state) return;
-			}
-			if (pl->_id != my_id)
-				pl->send_remove_player_packet(my_id);
-			
-			});
-
+		std::shared_ptr<SESSION> pl = objects[p_id].load();
+		if (pl == nullptr) continue;
+		if (pl->_id == _id) continue;
+		lock_guard<mutex> ll(pl->_s_lock);
+		if (ST_INGAME != pl->_state) continue;
+		pl->send_remove_player_packet(_id);
 	}
-
 	lock_guard<mutex> ll(_s_lock);
 	_state = ST_FREE;
-	objects.visit(_id, [](auto &apl) {
-		apl.second = nullptr;
-		});
+	objects[_id] = nullptr;
 }
 
 void SESSION::process_packet(char* packet)
@@ -200,18 +189,16 @@ void SESSION::process_packet(char* packet)
 			_state = ST_INGAME;
 		}
 		send_login_info_packet();
-		objects.visit_all([this](auto &item) {
-			shared_ptr<SESSION> ppl = item.second;
-			{
-				lock_guard<mutex> ll(ppl->_s_lock);
-				if (ST_INGAME != ppl->_state) return;
-			}
-			if (ppl->_id == _id) return;
-			if (false == can_see(ppl)) return;
+		for (const auto& pl : objects) {
+			shared_ptr<SESSION> ppl = pl.second;
+			if (ppl == nullptr) continue;
+			if (ST_INGAME != ppl->_state) continue;
+			if (ppl->_id == _id) continue;
+			if (false == can_see(ppl)) continue;
 			if (is_pc(ppl->_id)) ppl->send_add_player_packet(_id);
 			else WakeUpNPC(ppl->_id, _id);
 			send_add_player_packet(ppl->_id);
-			});
+		}
 		break;
 	}
 	case CS_MOVE: {
@@ -228,59 +215,62 @@ void SESSION::process_packet(char* packet)
 		_vl.lock();
 		unordered_set<int> old_vlist = _view_list;
 		_vl.unlock();
-		objects.visit_all([&near_list, this](auto& cl) {
+		for (const auto& cl : objects) {
 			shared_ptr<SESSION> ccl = cl.second;
-			if (ccl->_state != ST_INGAME) return;
-			if (ccl->_id == _id) return;
+			if (ccl == nullptr) continue;
+			if (ccl->_state != ST_INGAME) continue;
+			if (ccl->_id == _id) continue;
 			if (can_see(ccl))
 				near_list.insert(ccl->_id);
-			});
+		}
+		_vl.lock();
+		_view_list = near_list;
+		_vl.unlock();
 
 		send_move_packet(_id);
 
 		for (auto& pl : near_list) {
-			objects.visit(pl, [pl, this](auto& item) {
-				shared_ptr<SESSION> cpl = item.second;
-				if (is_pc(pl)) {
-					cpl->_vl.lock();
-					if (cpl->_view_list.count(_id)) {
-						cpl->_vl.unlock();
-						cpl->send_move_packet(_id);
-					}
-					else {
-						cpl->_vl.unlock();
-						cpl->send_add_player_packet(_id);
-					}
+			std::shared_ptr<SESSION> cpl = objects[pl];
+			if (cpl == nullptr) continue;
+			if (ST_INGAME != cpl->_state) continue;
+			if (cpl->_id == _id) continue;
+			if (cpl->_id != pl) continue;
+			if (is_pc(pl)) {
+				if (0 != old_vlist.count(_id)) {
+					cpl->send_move_packet(_id);
 				}
-				else WakeUpNPC(pl, _id);
-				});
-
-			if (old_vlist.count(pl) == 0)
-				send_add_player_packet(pl);
-		}
-
-		for (auto& pl : old_vlist)
-			if (0 == near_list.count(pl)) {
-				send_remove_player_packet(pl);
-				if (is_pc(pl)) {
-					objects.visit(pl, [this](auto &item) {
-						shared_ptr<SESSION> ppl = item.second;
-						ppl->send_remove_player_packet(_id);
-						});
+				else {
+					cpl->send_add_player_packet(_id);
+					send_add_player_packet(cpl->_id);
 				}
 			}
-	}
-				break;
-	}
-}
+			else {
+				send_add_player_packet(cpl->_id);
+				WakeUpNPC(pl, _id);
+			}
+		}
 
-// NPC 구현 세번째 방법  (실제로 많이 사용되는 방법)
-//   클래스 상속기능을 사용한다.
-//     SESSION은 NPC클래스를 상속받아서 네트워크 관련 기능을 추가한 형태로 정의한다.
-//       clients컨테이너를 objects컨테이너로 변경하고, 컨테이너는 NPC의 pointer를 저장한다.
-//      장점 : 메모리 낭비가 없다, 함수의 중복작성이 필요없다.
-//          (포인터로 관리되므로 player id의 중복사용 방지를 구현하기 쉬워진다 => Data Race 방지를 위한 추가 구현이 필요)
-//      단점 : 포인터가 사용되고, reinterpret_cast가 필요하다. (별로 단점이 안니다).
+		for (auto& pl : old_vlist) {
+			if (0 != near_list.count(pl)) continue;
+			std::shared_ptr<SESSION> cpl = objects[pl];
+			if (cpl == nullptr) continue;
+			if (ST_INGAME != cpl->_state) continue;
+			if (cpl->_id == _id) continue;
+			if (cpl->_id != pl) continue;
+			if (is_pc(pl)) cpl->send_remove_player_packet(_id);
+		}
+		break;
+	}
+	}
+
+	// NPC 구현 세번째 방법  (실제로 많이 사용되는 방법)
+	//   클래스 상속기능을 사용한다.
+	//     SESSION은 NPC클래스를 상속받아서 네트워크 관련 기능을 추가한 형태로 정의한다.
+	//       clients컨테이너를 objects컨테이너로 변경하고, 컨테이너는 NPC의 pointer를 저장한다.
+	//      장점 : 메모리 낭비가 없다, 함수의 중복작성이 필요없다.
+	//          (포인터로 관리되므로 player id의 중복사용 방지를 구현하기 쉬워진다 => Data Race 방지를 위한 추가 구현이 필요)
+	//      단점 : 포인터가 사용되고, reinterpret_cast가 필요하다. (별로 단점이 안니다).
+}
 
 void SESSION::send_move_packet(const shared_ptr<SESSION> &mover)
 {
@@ -296,32 +286,30 @@ void SESSION::send_move_packet(const shared_ptr<SESSION> &mover)
 
 void SESSION::send_move_packet(int c_id)
 {
-	objects.visit(c_id, [this, c_id](auto &item) {
-		shared_ptr<SESSION> mover = item.second;
-		SC_MOVE_OBJECT_PACKET p;
-		p.id = c_id;
-		p.size = sizeof(SC_MOVE_OBJECT_PACKET);
-		p.type = SC_MOVE_OBJECT;
-		p.x = mover->x;
-		p.y = mover->y;
-		p.move_time = mover->last_move_time;
-		do_send(&p);
-		});
+	shared_ptr<SESSION> mover = objects[c_id];
+	if (nullptr == mover) return;
+	SC_MOVE_OBJECT_PACKET p;
+	p.id = c_id;
+	p.size = sizeof(SC_MOVE_OBJECT_PACKET);
+	p.type = SC_MOVE_OBJECT;
+	p.x = mover->x;
+	p.y = mover->y;
+	p.move_time = mover->last_move_time;
+	do_send(&p);
 }
 
 void SESSION::send_add_player_packet(int c_id)
 {
 	SC_ADD_OBJECT_PACKET add_packet;
-	objects.visit(c_id, [&add_packet, c_id](auto &item) {
-		shared_ptr<SESSION> ppl = item.second;
-		SC_ADD_OBJECT_PACKET add_packet;
-		add_packet.id = c_id;
-		strcpy_s(add_packet.name, ppl->_name);
-		add_packet.size = sizeof(add_packet);
-		add_packet.type = SC_ADD_OBJECT;
-		add_packet.x = ppl->x;
-		add_packet.y = ppl->y;
-		});
+
+	shared_ptr<SESSION> ppl = objects[c_id];
+	if (nullptr == ppl) return;
+	add_packet.id = ppl->_id;
+	strcpy_s(add_packet.name, ppl->_name);
+	add_packet.size = sizeof(add_packet);
+	add_packet.type = SC_ADD_OBJECT;
+	add_packet.x = ppl->x;
+	add_packet.y = ppl->y;
 	_vl.lock();
 	_view_list.insert(c_id);
 	_vl.unlock();
@@ -346,41 +334,47 @@ int get_new_client_id()
 
 void WakeUpNPC(int npc_id, int waker)
 {
-	boost::asio::post([npc_id, waker]() {
-		objects.visit(npc_id, [waker](auto &item) {
-			shared_ptr<SESSION> cl = item.second;
-			cl->_ll.lock();
-			auto L = cl->_L;
-			lua_getglobal(L, "event_player_move");
-			lua_pushnumber(L, waker);
-			lua_pcall(L, 1, 0, 0);
-			lua_pop(L, 1);
-			cl->_ll.unlock();
-			});
+	shared_ptr<SESSION> cl = objects[npc_id];
+	if (nullptr == cl) return;
+
+	asio::post([npc_id, waker]() {
+		shared_ptr<SESSION> cl = objects[npc_id];
+		if (nullptr == cl) return;
+		cl->_ll.lock();
+		auto L = cl->_L;
+		lua_getglobal(L, "event_player_move");
+		lua_pushnumber(L, waker);
+		lua_pcall(L, 1, 0, 0);
+		//lua_pop(L, 1);
+		cl->_ll.unlock();
 		});
 
-	objects.visit(npc_id, [](auto &item) {
-		shared_ptr<SESSION> cl = item.second;
-		if (cl->_is_active) return;
-		bool old_state = false;
-		if (false == atomic_compare_exchange_strong(&cl->_is_active, &old_state, true))
+	if (cl->_is_active) return;
+	bool old_state = false;
+	if (false == atomic_compare_exchange_strong(&cl->_is_active, &old_state, true))
 			return;
+
+	asio::post([npc_id, waker]() {
+		shared_ptr<SESSION> cl = objects[npc_id];
+		if (nullptr == cl) return;
+		cl->do_npc_random_move();
+		timer_queue.push({ npc_id, chrono::system_clock::now() + chrono::seconds(1), EV_RANDOM_MOVE, waker });
 		});
-	TIMER_EVENT ev{ npc_id, chrono::system_clock::now(), EV_RANDOM_MOVE, 0 };
-	timer_queue.push(ev);
+
 }
 
 void SESSION::do_npc_random_move()
 {
 	unordered_set<int> old_vl;
 
-	objects.visit_all([&old_vl, this](auto &item) {
+	for (const auto& item : objects) {
 		shared_ptr<SESSION> pl = item.second;
+		if (nullptr == pl) continue;
 		if (ST_INGAME == pl->_state)
 			if (true == is_pc(pl->_id))
 				if (true == can_see(pl))
 					old_vl.insert(pl->_id);
-		});
+	}
 
 	switch (rand() % 4) {
 	case 0: if (x < (W_WIDTH - 1)) x++; break;
@@ -390,46 +384,46 @@ void SESSION::do_npc_random_move()
 	}
 
 	unordered_set<int> new_vl;
-	objects.visit_all([&new_vl, this](auto &item) {
+	for (const auto& item : objects) {
 		shared_ptr<SESSION> pl = item.second;
+		if (nullptr == pl) continue;
 		if (ST_INGAME == pl->_state)
 			if (true == is_pc(pl->_id))
 				if (true == can_see(pl))
 					new_vl.insert(pl->_id);
-		});
+	}
 
 	for (auto pl : new_vl) {
-		objects.visit(pl, [&old_vl, pl, this](auto &item) {
-			shared_ptr<SESSION> ppl = item.second;
-			if (0 == old_vl.count(pl)) {
-				// 플레이어의 시야에 등장
-				ppl->send_add_player_packet(_id);
+
+		shared_ptr<SESSION> ppl = objects[pl];
+		if (ppl == nullptr) continue;
+		if (0 == old_vl.count(pl)) {
+			// 플레이어의 시야에 등장
+			ppl->send_add_player_packet(_id);
+		}
+		else {
+			// 플레이어가 계속 보고 있음.
+			ppl->send_move_packet(_id);
+		}
+	}
+
+	for (auto pl : old_vl) {
+		if (0 == new_vl.count(pl)) {
+			shared_ptr<SESSION> ppl = objects[pl];
+			if (ppl == nullptr) continue;
+			ppl->_vl.lock();
+			if (0 != ppl->_view_list.count(_id)) {
+				ppl->_vl.unlock();
+				ppl->send_remove_player_packet(_id);
 			}
 			else {
-				// 플레이어가 계속 보고 있음.
-				ppl->send_move_packet(_id);
-			}
-			});
-		///vvcxxccxvvdsvdvds
-		for (auto pl : old_vl) {
-			if (0 == new_vl.count(pl)) {
-				objects.visit(pl, [this](auto &item) {
-					shared_ptr<SESSION> ppl = item.second;
-					ppl->_vl.lock();
-					if (0 != ppl->_view_list.count(_id)) {
-						ppl->_vl.unlock();
-						ppl->send_remove_player_packet(_id);
-					}
-					else {
-						ppl->_vl.unlock();
-					}
-					});
+				ppl->_vl.unlock();
 			}
 		}
 	}
 }
 
-void worker_thread(boost::asio::io_context *io_context)
+void worker_thread(asio::io_context *io_context)
 {
 	io_context->run();
 }
@@ -439,10 +433,9 @@ int API_get_x(lua_State* L)
 	int user_id =(int)lua_tointeger(L, -1);
 	lua_pop(L, 2);
 	int x = 0;
-	objects.visit(user_id, [&x](auto &item) {
-		shared_ptr<SESSION> cl = item.second;
+	shared_ptr<SESSION> cl = objects[user_id];
+	if (nullptr != cl)
 		x = cl->x;
-		});
 	lua_pushnumber(L, x);
 	return 1;
 }
@@ -452,10 +445,9 @@ int API_get_y(lua_State* L)
 	int user_id = (int)lua_tointeger(L, -1);
 	lua_pop(L, 2);
 	int y = 0;
-	objects.visit(user_id, [&y](auto &item) {
-		shared_ptr<SESSION> cl = item.second;
+		shared_ptr<SESSION> cl = objects[user_id];
+	if (nullptr != cl)
 		y = cl->y;
-		});
 	lua_pushnumber(L, y);
 	return 1;
 }
@@ -468,41 +460,40 @@ int API_SendMessage(lua_State* L)
 
 	lua_pop(L, 4);
 
-	objects.visit(user_id, [my_id, mess](auto &item) {
-		shared_ptr<SESSION> cl = item.second;
-		cl->send_chat_packet(my_id, mess);
-		});
+	shared_ptr<SESSION> cl = objects[user_id];
+	if (nullptr != cl) cl->send_chat_packet(my_id, mess);
 	return 0;
 }
 
-void InitializeNPC()
+void InitializeNPC(asio::io_context &io_context)
 {
-	cout << "NPC intialize begin.\n";
+	std::cout << "NPC intialize begin.\n";
 	for (int i = MAX_USER; i < MAX_USER + MAX_NPC; ++i) {
-		objects.visit(i, [i](auto &item) {
-			shared_ptr<SESSION> npc = item.second;
-			npc->x = rand() % W_WIDTH;
-			npc->y = rand() % W_HEIGHT;
-			npc->_id = i;
-			sprintf_s(npc->_name, "NPC%d", i);
-			npc->_state = ST_INGAME;
+		shared_ptr<SESSION> npc = make_shared<SESSION>(asio::ip::tcp::socket(io_context), i);
+		npc->x = rand() % W_WIDTH;
+		npc->y = rand() % W_HEIGHT;
+		npc->_id = i;
+		sprintf_s(npc->_name, "NPC%d", i);
+		npc->_state = ST_INGAME;
+		npc->_is_active = false;
 
-			auto L = npc->_L = luaL_newstate();
-			luaL_openlibs(L);
-			luaL_loadfile(L, "npc.lua");
-			lua_pcall(L, 0, 0, 0);
+		auto L = npc->_L = luaL_newstate();
+		luaL_openlibs(L);
+		luaL_loadfile(L, "npc.lua");
+		lua_pcall(L, 0, 0, 0);
 
-			lua_getglobal(L, "set_uid");
-			lua_pushnumber(L, i);
-			lua_pcall(L, 1, 0, 0);
-			// lua_pop(L, 1);// eliminate set_uid from stack after call
+		lua_getglobal(L, "set_uid");
+		lua_pushnumber(L, i);
+		lua_pcall(L, 1, 0, 0);
+		// lua_pop(L, 1);// eliminate set_uid from stack after call
 
-			lua_register(L, "API_SendMessage", API_SendMessage);
-			lua_register(L, "API_get_x", API_get_x);
-			lua_register(L, "API_get_y", API_get_y);
-			});
+		lua_register(L, "API_SendMessage", API_SendMessage);
+		lua_register(L, "API_get_x", API_get_x);
+		lua_register(L, "API_get_y", API_get_y);
+
+		objects[i] = npc;
 	}
-	cout << "NPC initialize end.\n";
+	std::cout << "NPC initialize end.\n";
 }
 
 void do_timer()
@@ -519,29 +510,28 @@ void do_timer()
 			}
 			switch (ev.event_id) {
 			case EV_RANDOM_MOVE:
-				int obj_id = ev.event_id;
-				boost::asio::post([obj_id]() {
-					objects.visit(obj_id, [obj_id](auto &item) {
-						shared_ptr<SESSION> npc = item.second;
-						bool keep_alive = false;
-						for (int j = 0; j < MAX_USER; ++j) {
-							objects.visit(j, [&keep_alive, &npc](auto &item) {
-								shared_ptr<SESSION> pl = item.second;
-								if (pl->_state == ST_INGAME)
-									if (pl->can_see(npc))
-										keep_alive = true;
-								});
-							if (true == keep_alive) break;
-						}
-						if (true == keep_alive) {
-							npc->do_npc_random_move();
-							TIMER_EVENT ev{ obj_id, chrono::system_clock::now() + 1s, EV_RANDOM_MOVE, 0 };
-							timer_queue.push(ev);
-						}
-						else {
-							npc->_is_active = false;
-						}
-						});
+				int obj_id = ev.obj_id;
+				asio::post([obj_id]() {
+					shared_ptr<SESSION> npc = objects[obj_id];
+					if (nullptr == npc) return;
+					bool keep_alive = false;
+					for (int j = 0; j < MAX_USER; ++j) {
+						shared_ptr<SESSION> pl = objects[j];
+						if (pl == nullptr) continue;
+						if (pl->_state == ST_INGAME)
+							if (pl->can_see(npc)) {
+								keep_alive = true;
+								break;
+							}
+					}
+					if (true == keep_alive) {
+						npc->do_npc_random_move();
+						TIMER_EVENT ev{ obj_id, chrono::system_clock::now() + 1s, EV_RANDOM_MOVE, 0 };
+						timer_queue.push(ev);
+					}
+					else {
+						npc->_is_active = false;
+					}
 					});
 				break;
 			}
@@ -554,7 +544,7 @@ void do_timer()
 class GAME_SERVER
 {
 public:
-	GAME_SERVER(boost::asio::io_context& io_context, const boost::asio::ip::tcp::endpoint& endpoint)
+	GAME_SERVER(asio::io_context& io_context, const asio::ip::tcp::endpoint& endpoint)
 		: acceptor_(io_context, endpoint)
 	{
 		do_accept();
@@ -564,7 +554,7 @@ private:
 	void do_accept()
 	{
 		acceptor_.async_accept(
-			[this](boost::system::error_code ec, boost::asio::ip::tcp::socket socket)
+			[this](asio::error_code ec, asio::ip::tcp::socket socket)
 			{
 				if (!ec)
 				{
@@ -577,8 +567,7 @@ private:
 					new_session->_id = new_id;
 					new_session->_name[0] = 0;
 					new_session->_prev_remain = 0;
-					objects.try_emplace(new_id, new_session);
-
+					objects[new_id] = new_session;
 					new_session->do_recv();
 				}
 
@@ -586,17 +575,14 @@ private:
 			});
 	}
 
-	boost::asio::ip::tcp::acceptor acceptor_;
+	asio::ip::tcp::acceptor acceptor_;
 };
 
 int main()
 {
-
-	InitializeNPC();
-
-	boost::asio::io_context io_context;
-	boost::asio::ip::tcp::endpoint endpoint(boost::asio::ip::tcp::v4(), PORT_NUM);
-
+	asio::io_context io_context;
+	asio::ip::tcp::endpoint endpoint(asio::ip::tcp::v4(), PORT_NUM);
+	InitializeNPC(io_context);
 	GAME_SERVER s{ io_context, endpoint };
 
 	vector <thread> worker_threads;
